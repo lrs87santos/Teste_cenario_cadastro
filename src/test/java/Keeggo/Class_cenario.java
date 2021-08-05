3621d8b71884bf99c4bfae4b19c8cd628e78f4da
lrs87santos <65510335+lrs87santos@users.noreply.github.com>
2021-08-05 13:10:05 -0300

    Add files via upload

---
+++ src/test/java/Keeggo/Class_cenario.java
@@ -0,0 +1,24 @@
+package Keeggo;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Class_cenario {
+
+	@Given("^que criei o Arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+		throw new Exception();
+
+	@When("^que criei o Arquivo corretamente$")
+		public void que_criei_o_arquivo_corretamente11() throws Throwable {
+			throw new Exception();
+
+	@Then("^que criei o Arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente1() throws Throwable {
+		throw new Exception();
+	}
+
+	
+	
+}
---
+++ src/test/java/Keeggo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Leonardo
+ *
+ */
+package Keeggo;
